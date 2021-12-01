5b3cdbaf6a2608520f330383b16a2cab14edfc2e
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-12-01 06:29:08 +0100

    day01

---
+++ 2021/day01/Program.cs
@@ -0,0 +1,28 @@
+﻿var depths = File.ReadAllLines("input.txt").Select(x => Convert.ToInt32(x)).ToList();
+Console.WriteLine(PartOne());
+Console.WriteLine(PartTwo());
+
+int PartOne()
+{
+  var previous = 0;
+  var increases = 0;
+  foreach (var d in depths)
+  {
+    if (d > previous) increases++;
+    previous = d;
+  }
+  return increases - 1;
+}
+
+int PartTwo()
+{
+  var previous = 0;
+  var increases = 0;
+  for (int i = 0; i < depths.Count() - 2; i++)
+  {
+    var d = depths[i] + depths[i + 1] + depths[i + 2];
+    if (d > previous) increases++;
+    previous = d;
+  }
+  return increases - 1;
+}
