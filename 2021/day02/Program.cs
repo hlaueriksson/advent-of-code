350e92dc7fd1f925ba667c4046fd08a230c37690
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-12-02 06:29:26 +0100

    day02

---
+++ 2021/day02/Program.cs
@@ -0,0 +1,46 @@
+﻿var commands = File.ReadLines("input.txt").Select(x => { var s = x.Split(' '); return new { Direction = s[0], Unit = Convert.ToInt32(s[1]) }; }).ToList();
+Console.WriteLine(PartOne());
+Console.WriteLine(PartTwo());
+
+int PartOne()
+{
+  int x = 0, y = 0;
+  foreach (var command in commands)
+  {
+    switch (command.Direction)
+    {
+      case "forward":
+        x += command.Unit;
+        break;
+      case "down":
+        y += command.Unit;
+        break;
+      case "up":
+        y -= command.Unit;
+        break;
+    }
+  }
+  return x * y;
+}
+
+int PartTwo()
+{
+  int x = 0, y = 0, aim = 0;
+  foreach (var command in commands)
+  {
+    switch (command.Direction)
+    {
+      case "forward":
+        x += command.Unit;
+        y += aim * command.Unit;
+        break;
+      case "down":
+        aim += command.Unit;
+        break;
+      case "up":
+        aim -= command.Unit;
+        break;
+    }
+  }
+  return x * y;
+}
