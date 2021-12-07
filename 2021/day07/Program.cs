1fd7eece357bda21c04a1b1c7a4c798537a49830
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-12-07 20:36:53 +0100

    day07

---
+++ 2021/day07/Program.cs
@@ -0,0 +1,44 @@
+﻿var crabs = File.ReadAllLines("input.txt").First().Split(',').Select(x => Convert.ToInt32(x)).ToList();
+var max = crabs.Max();
+
+Console.WriteLine(PartOne());
+Console.WriteLine(PartTwo());
+
+int PartOne()
+{
+  var fuel = int.MaxValue;
+  for (int i = 0; i < max; i++)
+  {
+    var result = 0;
+    foreach (var crab in crabs)
+    {
+      result += Math.Abs(i - crab);
+    }
+    if (result < fuel) fuel = result;
+  }
+  return fuel;
+}
+
+int PartTwo()
+{
+  var fuel = int.MaxValue;
+  for (int i = 0; i < max; i++)
+  {
+    var result = 0;
+    foreach (var crab in crabs)
+    {
+      result += GetCost(Math.Abs(i - crab));
+    }
+    if (result < fuel) fuel = result;
+  }
+  return fuel;
+  int GetCost(int steps)
+  {
+    var result = 0;
+    for (int i = 0; i <= steps; i++)
+    {
+      result += i;
+    }
+    return result;
+  }
+}
